c3e7aecb96be48eefd181d7618ef468487cccf6d
Marcos Suarez Sevares <marcosjss1234@gmail.com>
2026-04-23 19:59:29 +0200

    script

---
+++ src/test/java/com/dam/calculatorSSMJ/CalculatorSsmjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam.calculatorSSMJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorSsmjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
